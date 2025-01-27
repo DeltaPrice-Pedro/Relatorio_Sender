c00293d1c0c943744a7d40d71c506cb317a9e9a7
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2025-01-27 17:31:32 -0300

    add

--- code/index.py
+++ code/index.py
@@ -4,13 +4,15 @@ from pathlib import Path
 import json
 
 class Project:
-    def __init__(self, nome: str,inicio: datetime, termino: datetime, idealizador: str, aderencia: bool):
+    def __init__(self, nome: str, finish: int, thinker: str):
+        today = datetime.now()
         self.data = {
             "name": nome,
-            "termino": termino.strftime('%d/%m/%Y'),
-            "termino": termino.strftime('%d/%m/%Y'),
-            "idealizador": idealizador,
-            "aderencia": 'Utilizado' if aderencia else 'Inutilizado'
+            "start": today.strftime('%d/%m/%Y'),
+            "finish": (today + timedelta(days=finish)).strftime('%d/%m/%Y'),
+            "thinker": thinker,
+            "state": 'Inativo',
+            "grip": 'Inutilizado'
         }
         pass
     
@@ -28,21 +30,23 @@ class Resume:
             return json.load(file)
     
     def add(self, project: Project) -> str:
-        uuid = str(uuid.uuid4())
+        uuid_str = str(uuid.uuid4())
+        self.base_data[uuid_str] = json.loads(project.to_string())
+        
         with open(self.path, 'w') as file:
             json.dump(
-                project.to_string(),
+                self.base_data,
                 file,
                 ensure_ascii=False,
                 indent=2
             )
-        return f'{uuid} -> {project.to_string()}'
+        return f'{uuid_str} -> \n{'\n'.join(project.to_string().split(','))}'
 
 class Main:
     def __init__(self):
         self.resume = Resume()
-        self.menu()
         self.end = False
+        self.menu()
         pass
     
     def error(self, message: str, method):
@@ -53,9 +57,9 @@ class Main:
     
     def options(self) -> int:
         try:
-            print(f'{"#"*10}\n{"#"*10}Gerenciador de Relatório{"#"*10}\n{"#"*10}')
-            print('>> Projeto\n1) Criar\n2) Atualizar\n3) Apagar\n\n>> Enviar <<\n4) [Sair]')
-            return int(input())
+            print(f'{"#"*40}\n{"#"*7} Gerenciador de Relatório {"#"*7}\n{"#"*40}')
+            print('>> Projeto\n1) Criar\n2) Atualizar\n3) Apagar\n\n>> Relatório\n4) Visualizar\n5) Enviar\n\n6) Sair')
+            return int(input('RESPOSTA: '))
         except TypeError:
             return self.error('Tipo errado', self.options)
             
@@ -63,45 +67,33 @@ class Main:
         try:
             while(self.end == False):
                 answer = self.options()
+                print('\n')
                 if answer == 1:
-                    today = datetime.now()
-                    name, thinker, finish  = self.create_project()
-                    proj_add = self.resume.add(Project(
-                        name,
-                        today,
-                        today + timedelta(days=finish),
-                        thinker, 
-                        False
-                    ))
+                    proj_add = self.resume.add(self.create_project())
+                    print('\n--Projeto Adcionado--')
                     print(proj_add)
                     input()
                 elif answer == 2:
                     ...
                 elif answer == 3:
                     ...
-                elif answer == 4:
-                    end = True
+                elif answer == 5:
+                    self.end = True
                 else:
                     raise Exception('Fora das opções disponíveis')
         except Exception as e:
-            self.error(e.__str__, self.menu)
+            self.error(e.__str__(), self.menu)
             
     def create_project(self):
         try:
-            print('Nome: ')
-            name = input()
-            print('Idalizador: ')
-            thinker = input()
-            print('Previsão de término: ')
-            finish = int(input())
-            return name, thinker, finish
+            name = input('Nome: ')
+            thinker = input('Idealizador: ')
+            finish = int(input('Previsão de término: '))
+            return Project(name, finish, thinker)
         except TypeError:
             return self.error('Tipo errado', self.create_project)
+        except Exception as e:
+            return self.error(e.__str__(), self.create_project)
         
-        
-        
-            
-        
-
 if __name__ == '__main__':
     Main()
